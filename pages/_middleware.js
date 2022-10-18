ed4369b70606cd70d577e43d4a5156ded9e4ab22
haani0090 <haani0090>
2022-10-17 20:46:58 -0700

    first commit

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -10,7 +10,7 @@ export async function middleware(req) {
     });
     // You could also check for any property on the session object,
     // like role === "admin" or name === "John Doe", etc.
-    if (!session) return NextResponse.redirect("/");
+    if (!session) return NextResponse.redirect("/home");
     // If user is authenticated, continue.
   }
 }
